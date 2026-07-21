38957ae19ea129cfe68561bb69e3b2cfece8cc87
brunoviveirosdutra-ops <brunoviveirosdutra@gmail.com>
2026-07-21 13:59:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
 /* ======================================================
    SEGURANÇA
 ====================================================== */
